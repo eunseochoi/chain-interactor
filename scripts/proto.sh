0c052dcd5489899208af61a24183e9db93ea8162
Ellen Choi <ellen@polyweave.xyz>
2023-06-01 16:20:58 -0400

    Arbitrum proto definition

--- scripts/proto.sh
+++ scripts/proto.sh
@@ -3,4 +3,5 @@ protoc -I=. --go_opt=paths=source_relative --go_out=protos/go protos/chains/bina
 protoc -I=. --go_opt=paths=source_relative --go_out=protos/go protos/chains/ethereum/*.proto
 protoc -I=. --go_opt=paths=source_relative --go_out=protos/go protos/chains/optimism/*.proto
 protoc -I=. --go_opt=paths=source_relative --go_out=protos/go protos/chains/polygon/*.proto
+protoc -I=. --go_opt=paths=source_relative --go_out=protos/go protos/chains/arbitrum/*.proto
 protoc -I=. --go_opt=paths=source_relative --go_out=protos/go protos/shared/*.proto
